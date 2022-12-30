46343dd81b4702bc5f26a51312a9a485a853c7c7
VitaPetrushyna <99089205+VitaPetrushyna@users.noreply.github.com>
2022-12-31 01:01:38 +0200

    add imageBackground and input

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>З новим роком!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/images/painting-mountains.jpg")}
+      >
+        <TextInput style={styles.input} textAlign={"center"} />
+      </ImageBackground>
     </View>
   );
 }
@@ -15,7 +25,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    // alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#f0f8ff",
+    height: 40,
+    borderRadius: 6,
+    marginHorizontal: 40,
+    color: "#f0f8ff",
   },
 });
